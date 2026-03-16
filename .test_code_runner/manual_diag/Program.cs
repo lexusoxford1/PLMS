ef871320985403e5e5eff15979a141d06c1ecc0d
CodexSandboxOffline <keithpelonio111@gmail.com>
2026-03-16 16:40:42 +0800

    Add test code runner files

---
+++ .test_code_runner/manual_diag/Program.cs
@@ -0,0 +1,20 @@
+class Program
+{
+    static void Main()
+    {
+        int score = 84;
+
+        if (score >= 90)
+        {
+            Console.WriteLine("Excellent");
+        }
+        else if (score >= 75)
+        {
+            Console.WriteLine("Passed");
+        }
+        else
+        {
+            Console.WriteLine("Needs improvement");
+        }
+    }
+}
---
+++ .test_code_runner/manual_sample.cs
@@ -0,0 +1,16 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        int score = 84;
+        if (score >= 75)
+        {
+            Console.WriteLine("Passed");
+        }
+        else
+        {
+            Console.WriteLine("Study and try again.");
+        }
+    }
+}
